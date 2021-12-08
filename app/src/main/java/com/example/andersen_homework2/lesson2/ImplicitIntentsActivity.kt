33a4b7b103e2c38576fb85b4ae69372c70412ff7
Ksenia Karamysheva <ya.ksushki@yandex.ru>
2021-12-08 10:43:50 +0300

    commit2

--- app/src/main/java/com/example/andersen_homework2/lesson2/ImplicitIntentsActivity.kt
+++ app/src/main/java/com/example/andersen_homework2/lesson2/ImplicitIntentsActivity.kt
@@ -1,12 +1,17 @@
 package com.example.andersen_homework2.lesson2
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.app.ShareCompat
+import androidx.core.content.ContextCompat
 import com.example.andersen_homework2.databinding.ActivityImplicitsIntentsBinding
 
 class ImplicitIntentsActivity : AppCompatActivity() {
@@ -63,12 +68,30 @@ class ImplicitIntentsActivity : AppCompatActivity() {
             .startChooser()
     }
 
+    private fun checkPermission(permission: String, requestCode: Int) {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                permission
+            ) == PackageManager.PERMISSION_DENIED
+        ) {
+            ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
+        } else {
+            Toast.makeText(this, "Permission already granted", Toast.LENGTH_SHORT).show()
+        }
+    }
+
     private fun takePhoto() {
+
+        checkPermission(Manifest.permission.CAMERA, CAMERA_PERMISSION_CODE)
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         if (intent.resolveActivity(packageManager) != null) {
             startActivity(intent)
         } else {
             Log.d("ImplicitIntents", "Can't handle this address!")
         }
+    }
+
+    companion object {
+        const val CAMERA_PERMISSION_CODE = 1
     }
 }
